257bcd414409904cc991c1cbeff4d36844c05218
Miguel <santosmiguel25@gmail.com>
2017-09-11 22:59:15 +0100

    Added 'getComponent()' method

--- src/gui/GenericScene.java
+++ src/gui/GenericScene.java
@@ -2,6 +2,7 @@ package gui;
 
 import gui.Util.*;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -20,10 +21,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public abstract class GenericScene {
-	
+
 	/* Top path of the this scene */
 	private String topPath;
-	
+
 	/* This scene's resources and settings */
 	protected String sceneTitle;
 	protected String sceneTitleNormalized;
@@ -31,7 +32,7 @@ public abstract class GenericScene {
 	private final String sceneCSSFile  = "styles.css";
 	private final String resBundleFile = "strings";
 	private String localeName = JFX.DEFAULT_LOCALE;
-	
+
 	/* Important scene objects */
 	protected ResourceBundle bundle;
 	protected FXMLLoader loader = null;
@@ -40,26 +41,26 @@ public abstract class GenericScene {
 	protected Parent root;
 	protected GenericController controller;
 	protected boolean hasStylesheet = false;
-	
+
 	private MultiHashMap<String, Stage> childStagesInstantiated = null;
-		
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public GenericScene(Stage stage) {
-		
+
 		childStagesInstantiated = new MultiHashMap<String, Stage>();
-		
+
 		try {
 			String packagePath = this.getClass().getCanonicalName();
-			
+
 			/* Set the top path */
 			topPath = packagePath.substring(0, packagePath.lastIndexOf(".")).replace(".", "/") + "/";
-			
+
 			/* The title of the scene */
 			sceneTitle = packagePath.substring(0, packagePath.lastIndexOf("."));
 			sceneTitle = sceneTitle.substring(sceneTitle.lastIndexOf(".") + 1);
 			sceneTitle = sceneTitle.substring(0, 1).toUpperCase() + sceneTitle.substring(1);
 			sceneTitleNormalized = sceneTitle.toLowerCase(); 
-			
+
 			/* Initialize Controller */
 			Class controllerClass;
 			try {
@@ -67,14 +68,14 @@ public abstract class GenericScene {
 			} catch(Exception e) {
 				controllerClass = null;
 			}
-			
+
 			/* Load resources and initialize FXML loader */
 			try {
 				bundle = ResourceBundle.getBundle(topPath + resBundleFile, new Locale(localeName));
 			} catch(Exception e) {
 				bundle = null;
 			}
-			
+
 			String fxmlFilepath = topPath + sceneFile;
 			if(Util.resourceExists(fxmlFilepath)) {
 				/* Load the FXML file into Parent node */
@@ -82,7 +83,7 @@ public abstract class GenericScene {
 					loader = new FXMLLoader(getClass().getResource("/" + fxmlFilepath), bundle);
 				else
 					loader = new FXMLLoader(getClass().getResource("/" + fxmlFilepath));
-				
+
 				/* Instantiate and set controller */
 				if(controllerClass != null) {
 					controller =
@@ -94,7 +95,7 @@ public abstract class GenericScene {
 				/* Set parent node and scene */
 				root = loader.load();
 				scene = new Scene(root);
-				
+
 				/* Initialize everything else */
 				if(controllerClass != null) {
 					controller.setScene(scene);
@@ -119,7 +120,7 @@ public abstract class GenericScene {
 				scene.getStylesheets().add(getClass().getResource("/" + stylesheetFilepath).toExternalForm());
 				hasStylesheet = true;
 			}
-			
+
 			stage.setTitle(sceneTitle + " - " + JFX.APP_NAME);		
 			stage.setScene(scene);
 
@@ -132,16 +133,16 @@ public abstract class GenericScene {
 
 	@SuppressWarnings("rawtypes")
 	class CustomNamedHandler implements EventHandler {
-		
+
 		private final Object objectReference;
 		private final String handlerName;
 		private Method handlerMethod = null;
-		
+
 		public CustomNamedHandler(Object objectReference, String handlerName) {
 			this.objectReference = objectReference;
 			this.handlerName = handlerName;
 		}
-		
+
 		@Override
 		public void handle(Event event) {
 			try {
@@ -156,29 +157,29 @@ public abstract class GenericScene {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 	public void addChildStage(Stage stage, String childStageName) {
 		childStagesInstantiated.put(childStageName, stage);
 	}
-	
+
 	public void removeChildStage(Stage stageObjectReference) {
 		childStagesInstantiated.values().remove(stageObjectReference);
 	}
-	
+
 	public Stage getChildStage(String childStageName) {
 		return childStagesInstantiated.get(childStageName, 0);
 	}
-	
+
 	public List<Stage> getChildrenStages() {
 		return new ArrayList<Stage>(childStagesInstantiated.values());
 	}
-	
+
 	public boolean isChildStageInstantiated(String childStage) {
 		return childStagesInstantiated.containsKey(childStage);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	protected <T extends Event> boolean addEvent(Node node, EventType<T> evt, String handlerName) {
 		if(controller == null)
@@ -186,7 +187,7 @@ public abstract class GenericScene {
 		node.addEventHandler(evt, new CustomNamedHandler(controller, handlerName));
 		return true;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	protected <T extends Event> boolean addEvent(Node node, EventType<T> evt) {
 		if(controller == null)
@@ -194,7 +195,7 @@ public abstract class GenericScene {
 		node.addEventHandler(evt, controller);
 		return true;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	protected <T extends Event> boolean removeEvent(Node node, EventType<T> evt) {
 		if(controller == null)
@@ -203,16 +204,44 @@ public abstract class GenericScene {
 		return true;
 	}
 
+	@SuppressWarnings("unchecked")
+	protected <T> T getComponent(String componentID) {
+		T n = null;
+
+		try {
+			/* Search for this node on the Controller class */
+			Field f = controller.getClass().getDeclaredField(componentID);
+			f.setAccessible(true);
+			n = (T) f.get(controller);
+		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+			/* Try looking for this node through the root node instead */
+			char firstChar = componentID.charAt(0);
+
+			if(firstChar != '#' && firstChar != '.') {
+				/* Use ID selector */
+				n = (T) root.lookup("#" + componentID);
+				if(n == null) {
+					/* Try a class selector */
+					n = (T) root.lookup("." + componentID);
+				}
+			} else {
+				n = (T) root.lookup(componentID);
+			}
+		}
+
+		return n;
+	}
+
 	public void show() {
 		stage.show();
 		afterShow();
 	}
-	
+
 	protected void afterShow() {
 		/* Nothing to do.
 		 * Sub class can override this optionally */
 	}
-	
+
 	public void close() {
 		stage.close();
 	}
